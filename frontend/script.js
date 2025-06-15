94a43aa12cd3565ace8fb0de3331a5e0346db4b8
Bakwowi Junior <juniorbakwowi@gmail.com>
2025-06-15 13:46:22 +0200

    Implement initial structure and functionality for News Scraper application with event listeners and dynamic source selection

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,12 @@
+const selectSource = document.getElementById("select-source");
+const fetchNewsButton = document.getElementById("fetch-news");
+const searchInput = document.getElementById("search-news-input");
+const searchButton = document.getElementById("search-button");
+const exportCVS = document.getElementById("export-cvs");
+const exportJSON = document.getElementById("export-JSON");
+
+
+
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const response = await fetch("http://127.0.0.1:8000", {
@@ -17,6 +26,9 @@ document.addEventListener("DOMContentLoaded", async () => {
                 }
             });
             const data = await response.json();
+            selectSource.innerHTML += data.map(source => {
+                return `<option value="${source}">${source}</option>`;
+            }).join('');
             console.log(data);
         } catch (error) {
             console.error("Error fetching data:", error);
@@ -25,3 +37,37 @@ document.addEventListener("DOMContentLoaded", async () => {
         console.error("Error fetching data:", error);
     }
 });
+
+selectSource.addEventListener("change", (e) => {
+    console.log("selected source => ", e.target.value)
+});
+
+fetchNewsButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e.target.textContent, " => ", selectSource.value);
+});
+
+// If you want to handle pressing Enter in the input:
+searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        searchNews();
+    }
+});
+
+searchButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchNews();
+});
+
+exportCVS.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+});
+
+exportJSON.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+});
+
+const searchNews = () => {
+    return console.log("Search triggered => ", searchInput.value);
+};
