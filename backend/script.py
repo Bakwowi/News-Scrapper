d2a8033fbb48c15f6e701624e1ff6b0e55d9afae
Bakwowi Junior <juniorbakwowi@gmail.com>
2025-06-15 16:37:51 +0200

    Enhance News Scraper functionality by adding source fetching and error handling; update sources.json to include a default source; improve frontend search capabilities and user experience.

--- backend/script.py
+++ backend/script.py
@@ -2,28 +2,34 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json
 from fastapi.middleware.cors import CORSMiddleware
+import feedparser
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins="http://127.0.0.1:5500",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+sources = []
+source_names = []
 
 @app.get("/")
 def read_root():
     return {"message": "Hi 👋 from News Scrapper's server"}
 
 @app.get("/sources")
-def read_item():
+def fetch_all_news_sources():
     try:
         import os
         file_path = os.path.join(os.path.dirname(__file__), "sources.json")
+        global sources
         with open(file_path, "r") as file:
             sources = json.load(file)
+        global source_names
         source_names = []
         for i in sources:
             source_names.append(i.get("name", ""))
@@ -35,4 +41,24 @@ def read_item():
     except Exception as e:
         return {"error": f"An unexpected error occurred: {str(e)}"}
 
-print("hi from the server")
+
+
+@app.get("/sources/{source}")
+def fetch_data_from_source(source: str):
+    url = ""
+    for i in sources:
+        if i["name"] == source:
+            url = i["url"]
+    
+    feed = feedparser.parse(url)
+    data = []
+    for entry in feed.entries:
+        data.append({
+            "Title": entry.title,
+            "Link": entry.link,
+            "Published": entry.get('published', 'N/A'),
+            "Summary": entry.get('summary', 'No summary')
+        })
+    return data
+        
+
--- frontend/script.js
+++ frontend/script.js
@@ -4,8 +4,9 @@ const searchInput = document.getElementById("search-news-input");
 const searchButton = document.getElementById("search-button");
 const exportCVS = document.getElementById("export-cvs");
 const exportJSON = document.getElementById("export-JSON");
+const tableBody = document.getElementById("table-body");
 
-
+let news = [];
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
@@ -31,10 +32,10 @@ document.addEventListener("DOMContentLoaded", async () => {
             }).join('');
             console.log(data);
         } catch (error) {
-            console.error("Error fetching data:", error);
+            console.error("Error fetching sources:", error);
         }
     } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error connecting to the server:", error);
     }
 });
 
@@ -42,22 +43,51 @@ selectSource.addEventListener("change", (e) => {
     console.log("selected source => ", e.target.value)
 });
 
-fetchNewsButton.addEventListener("click", (e) => {
+fetchNewsButton.addEventListener("click", async (e) => {
     e.preventDefault();
-    console.log(e.target.textContent, " => ", selectSource.value);
+    
+    if(selectSource.value.trim() == ""){
+        console.error("Please select a source");
+        return
+    }
+
+    try{
+    const response = await fetch(`http://127.0.0.1:8000/sources/${selectSource.value}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    data = await response.json();
+    news = []
+    news = data
+
+    tableBody.innerHTML = ""
+    tableBody.innerHTML += news.map(item => {
+        return `<tr>
+                    <td>${item.Title}</td>
+                    <td>${item.Link}</td>
+                    <td>${item.Published}</td>
+                </tr>`
+    }).join('')
+    console.log(data);
+    }
+    catch(error) {
+        console.error("Error fetching source news", error);
+    }
 });
 
 // If you want to handle pressing Enter in the input:
 searchInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         e.preventDefault();
-        searchNews();
+        searchNews(searchInput.value);
     }
 });
 
 searchButton.addEventListener("click", (e) => {
     e.preventDefault();
-    searchNews();
+    searchNews(searchInput.value);
 });
 
 exportCVS.addEventListener("click", (e) => {
@@ -68,6 +98,47 @@ exportJSON.addEventListener("click", (e) => {
     console.log(e.target.textContent);
 });
 
-const searchNews = () => {
+const searchNews = (query) => {
+    if(query.trim() == ""){
+        console.error("Please enter a query")
+        return
+    }
+    if(news.length < 1){
+        console.error("No news to search from. Search news first");
+        return
+    }
+    let foundNews = []
+    news.forEach((item, index) => {
+        if(item.Title.toLowerCase().includes(query.toLowerCase())){
+            foundNews.push(item);
+        };
+    });
+    
+    if(foundNews.length > 0){
+        tableBody.innerHTML = "";
+        tableBody.innerHTML += foundNews.map(item => {
+            return `<tr>
+                        <td>${item.Title}</td>
+                        <td>${item.Link}</td>
+                        <td>${item.Published}</td>
+                    </tr>`
+        }).join('');
+    }
+    else{
+        tableBody.innerHTML = "";
+        tableBody.innerHTML += news.map(item => {
+            return `<tr>
+                        <td>${item.Title}</td>
+                        <td>${item.Link}</td>
+                        <td>${item.Published}</td>
+                    </tr>`
+        }).join('');
+        console.log("No news found")
+    }
     return console.log("Search triggered => ", searchInput.value);
 };
+
+
+let title = "The fujha;ri rfofia roioiraona knvljser;n ;arjrva;jrubefv ";
+
+// console.log(title.toLowerCase().includes("the"));
