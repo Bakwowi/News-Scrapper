795c671fbfd68a89236236435bfd9e7b52b6ad9a
Bakwowi Junior <juniorbakwowi@gmail.com>
2025-06-27 21:53:56 +0200

    Refactor source management: update input fields for adding sources in index.html, enhance event listener for source selection in script.js, and improve UI styling in style.css for better user experience.

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,94 @@
-const selectSource = document.getElementById("select-source");
-const fetchNewsButton = document.getElementById("fetch-news");
 const searchInput = document.getElementById("search-news-input");
 const searchButton = document.getElementById("search-button");
+
+const sourceNameInput = document.getElementById("input-source-name");
+const sourceUrlInput = document.getElementById("input-source-url");
+const addNewSourceBtn = document.getElementById("add-new-source");
+
+const sourceContainer = document.querySelector(".sources");
+// const sources = document.querySelectorAll(".sources .source");
+
 const exportCVS = document.getElementById("export-cvs");
 const exportJSON = document.getElementById("export-JSON");
-const tableBody = document.getElementById("table-body");
+
 const dialogBox = document.getElementById("dialog-box");
 
 let headlines = [];
+
+const outputToDialogBox = (message, type) => {
+  if (type == "danger") {
+    dialogBox.textContent = message;
+    if (!dialogBox.classList.contains("danger")) {
+      dialogBox.classList.add("danger");
+    }
+  } else if (type == "success") {
+    dialogBox.textContent = message;
+    if (dialogBox.classList.contains("danger")) {
+      dialogBox.classList.remove("danger");
+    }
+  }
+};
+
+const AddSourceEventListener = () => {
+  const sources = document.querySelectorAll(".sources .source");
+  sources.forEach((source) => {
+    source.addEventListener("click", async () => {
+      // e.preventDefault();
+      console.log("hi there");
+
+      try {
+        outputToDialogBox("Fetching headlines.... ", "success");
+
+        const response = await fetch(
+          `http://127.0.0.1:8000/sources/${source.textContent}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        data = await response.json();
+        headlines = [];
+        headlines = data;
+        console.log(headlines);
+        const cardsContainer = document.querySelector(".news-cards");
+        cardsContainer.innerHTML = "";
+        if (headlines.length === 0) {
+          console.log("there are no headlines");
+          cardsContainer.innerHTML = "No headlines found for this source.";
+          // outputToDialogBox("")
+        } else {
+          // console.log(headlines);
+          cardsContainer.innerHTML += headlines
+          .map((item) => {
+              return `
+                 <div class="card autoshow">
+                <h1>
+                  ${item.Title}
+                </h1>
+                <p>
+                 ${item.Summary}
+                </p>
+                <div class="info">
+                  <span>${source.textContent}</span>.
+                  <span>${item.Published}</span>
+                </div>
+              </div>`;
+            })
+            .join("");
+        }
+        outputToDialogBox(
+          `${headlines.length} headlines have been successfully fetched`,
+          "success"
+        );
+      } catch (error) {
+        outputToDialogBox("Error fetching source headlines", "danger");
+        console.error("Error fetching source headlines", error);
+      }
+    });
+  });
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
@@ -27,12 +108,61 @@ document.addEventListener("DOMContentLoaded", async () => {
         },
       });
       const data = await response.json();
-      selectSource.innerHTML += data
-        .map((source) => {
-          return `<option value="${source}">${source}</option>`;
-        })
-        .join("");
+      sourceContainer.innerHTML +=
+        data
+          .map((source) => {
+            return `<button class="source">${source}</button>`;
+          })
+          .join("") +
+        `<button class="source" id="add-source-btn-2"><svg
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#ffffff"
+                    stroke="#ffffff"
+                    width="20"
+                    height="20"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <title></title>
+                      <g id="Complete">
+                        <g data-name="add" id="add-2">
+                          <g>
+                            <line
+                              fill="none"
+                              stroke="#ffffff"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              x1="12"
+                              x2="12"
+                              y1="19"
+                              y2="5"
+                            ></line>
+                            <line
+                              fill="none"
+                              stroke="#ffffff"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              x1="5"
+                              x2="19"
+                              y1="12"
+                              y2="12"
+                            ></line>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                  Add source</button>`;
       console.log(data);
+      AddSourceEventListener();
     } catch (error) {
       console.error("Error fetching sources:", error);
       outputToDialogBox("Error fetching sources", "danger");
@@ -40,58 +170,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   } catch (error) {
     console.error("Error connecting to the server:", error);
     outputToDialogBox("Error connecting to the server", "danger");
-  }
-});
-
-selectSource.addEventListener("change", (e) => {
-  console.log("selected source => ", e.target.value);
-});
-
-fetchNewsButton.addEventListener("click", async (e) => {
-  e.preventDefault();
-
-  if (selectSource.value.trim() == "") {
-    outputToDialogBox("Please select a source", "danger");
-    console.error("Please select a source");
-    return;
-  }
-
-  try {
-    outputToDialogBox("Fetching headlines.... ", "success");
-
-    const response = await fetch(
-      `http://127.0.0.1:8000/sources/${selectSource.value}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    data = await response.json();
-    headlines = [];
-    headlines = data;
-
-    tableBody.innerHTML = "";
-    if (headlines.length === 0) {
-      console.log("there are no headlines")
-      tableBody.innerHTML = `<tr><td colspan="3">No headlines found for this source.</td></tr>`;
-    } else {
-      console.log(headlines);
-      tableBody.innerHTML += headlines.map((item) => {
-          return `<tr>
-              <td>${item.Title}</td>
-              <td>${item.Link}</td>
-              <td>${item.Published}</td>
-            </tr>`;
-        })
-        .join("");
-    }
-    console.log(data);
-    outputToDialogBox(`${headlines.length} headlines have been successfully fetched`, "success");
-  } catch (error) {
-    outputToDialogBox("Error fetching source headlines", "danger");
-    console.error("Error fetching source headlines", error);
   }
 });
 
@@ -111,7 +189,10 @@ searchButton.addEventListener("click", (e) => {
 exportCVS.addEventListener("click", (e) => {
   try {
     if (headlines.length < 1) {
-      outputToDialogBox("No headlines to export. Fetch headlines first", "danger");
+      outputToDialogBox(
+        "No headlines to export. Fetch headlines first",
+        "danger"
+      );
       console.error("No headlines to export. Fetch headlines first");
       return;
     }
@@ -128,7 +209,10 @@ exportCVS.addEventListener("click", (e) => {
     a.click();
     URL.revokeObjectURL(url);
 
-    outputToDialogBox("The headlines have been successfully exported to 'Headlines.csv'", "success");
+    outputToDialogBox(
+      "The headlines have been successfully exported to 'Headlines.csv'",
+      "success"
+    );
     console.log(
       "The headlines have been successfully exported to 'Headlines.csv'"
     );
@@ -141,7 +225,10 @@ exportCVS.addEventListener("click", (e) => {
 exportJSON.addEventListener("click", (e) => {
   try {
     if (headlines.length < 1) {
-      outputToDialogBox("No headlines to export. Fetch headlines first", "danger");
+      outputToDialogBox(
+        "No headlines to export. Fetch headlines first",
+        "danger"
+      );
       console.error("No headlines to export. Fetch headlines first");
       return;
     }
@@ -156,7 +243,10 @@ exportJSON.addEventListener("click", (e) => {
     a.click();
     URL.revokeObjectURL(url);
 
-    outputToDialogBox("The headlines have been successfully exported into a JSON file", "success");
+    outputToDialogBox(
+      "The headlines have been successfully exported into a JSON file",
+      "success"
+    );
     console.log(
       "The headlines have been successfully exported into a JSON file"
     );
@@ -173,7 +263,10 @@ const searchNews = (query) => {
     return;
   }
   if (headlines.length < 1) {
-    outputToDialogBox("No news to search from. Fetch headlines first", "danger");
+    outputToDialogBox(
+      "No news to search from. Fetch headlines first",
+      "danger"
+    );
     console.error("No news to search from. Fetch headlines first");
     return;
   }
@@ -195,7 +288,10 @@ const searchNews = (query) => {
                     </tr>`;
       })
       .join("");
-    outputToDialogBox(`${foundHeadlines.length} headline(s) found for "${query}"`, "success");
+    outputToDialogBox(
+      `${foundHeadlines.length} headline(s) found for "${query}"`,
+      "success"
+    );
   } else {
     tableBody.innerHTML += headlines
       .map((item) => {
@@ -206,23 +302,11 @@ const searchNews = (query) => {
                     </tr>`;
       })
       .join("");
-    outputToDialogBox(`No headlines found for "${query}". Showing all headlines.`, "danger");
+    outputToDialogBox(
+      `No headlines found for "${query}". Showing all headlines.`,
+      "danger"
+    );
     console.log("No headlines found");
   }
   return console.log("Search triggered => ", searchInput.value);
-};
-
-const outputToDialogBox = (message, type) => {
-  if(type == "danger"){
-   dialogBox.textContent = message;
-      if (!dialogBox.classList.contains("danger")) {
-        dialogBox.classList.add("danger");
-      }
-  }
-  else if(type == "success"){
-    dialogBox.textContent = message;
-    if (dialogBox.classList.contains("danger")) {
-      dialogBox.classList.remove("danger");
-    }
-  }
 };
